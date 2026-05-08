2dfac2fed03cc7121b74c66b8b38dbb5fc4db976
livingsarkwah <159113494+livingsarkwah@users.noreply.github.com>
2026-05-08 09:52:22 +0000

    Initialize

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div className="footer">
+            <img src="/src/assets/Twitter icon.png" alt="twitter icon" />
+            <img src="/src/assets/Facebook icon.png" alt="facebook icon" />
+            <img src="/src/assets/Instagram icon.png" alt="instagram icon" />
+            <img src="/src/assets/GitHub icon.png" alt="github icon" />
+        </div>
+    )
+}
---
+++ components/Info.jsx
@@ -0,0 +1,24 @@
+export default function Info() {
+    return (
+        <div className="info">
+            <div className="info-profile">
+                <img src="/src/assets/myImage.png" alt="profile-pic" className="info-profile-pic" />
+            </div>
+            <div className="info-text">
+                <h2 className="info-name">Sarkwah Abdul Razak</h2>
+                <h4 className="info-title">Frontend Developer</h4>
+                <p className="info-website">sarkwah.website</p>
+                <div className="links">
+                    <a href="http:/gmail.com" className="email-link">
+                        <img src="/src/assets/Mail.png" alt="mail icon" />
+                        <span>Email</span>
+                    </a>
+                    <a href="http:/linkedin.com" className="linkedin-link">
+                        <img src="/src/assets/linkedIn.png" alt="LinkedIn icon" />
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
