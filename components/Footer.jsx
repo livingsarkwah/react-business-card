7371f182c5237d9710784cd092280f7204b6d18b
livingsarkwah <159113494+livingsarkwah@users.noreply.github.com>
2026-05-09 16:26:05 +0000

    Responsiveness incoporated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,18 @@
 export default function Footer() {
     return (
         <div className="footer">
-            <img src="/Twitter.png" alt="twitter icon" />
-            <img src="/Facebook.png" alt="facebook icon" />
-            <img src="/Instagram.png" alt="instagram icon" />
-            <img src="/GitHub.png" alt="github icon" />
+            <a href="https://x.com/livingsarkwah" target="_blank" rel="noopener noreferrer">
+                <img src="/Twitter.png" alt="twitter icon" />
+            </a>
+            <a href="https://www.facebook.com/share/1B3LuZ1v1G/" target="_blank" rel="noopener noreferrer">
+                <img src="/Facebook.png" alt="facebook icon" />
+            </a>
+            <a href="https://www.instagram.com/livinggraphix?igsh=dW84Mzc1bnZ3ajlm" target="_blank" rel="noopener noreferrer">
+                <img src="/Instagram.png" alt="instagram icon" />
+            </a>
+            <a href="https://github.com/livingsarkwah" target="_blank" rel="noopener noreferrer">
+                <img src="/GitHub.png" alt="github icon" />
+            </a>
         </div>
     )
 }
--- components/Info.jsx
+++ components/Info.jsx
@@ -1,24 +1,24 @@
 export default function Info() {
     return (
-        <div className="info">
+        <>
             <div className="info-profile">
                 <img src="/myImage.png" alt="profile-pic" className="info-profile-pic" />
             </div>
             <div className="info-text">
                 <h2 className="info-name">Sarkwah Abdul Razak</h2>
-                <h4 className="info-title">Frontend Developer</h4>
+                <h4 className="info-title">Fullstack Developer | Graphic Designer</h4>
                 <p className="info-website">sarkwah.website</p>
                 <div className="links">
-                    <a href="http:/gmail.com" className="email-link">
+                    <a href="mailto:abdulsarkwah@gmail.com" className="email-link">
                         <img src="/Mail.png" alt="mail icon" />
                         <span>Email</span>
                     </a>
-                    <a href="http:/linkedin.com" className="linkedin-link">
+                    <a href="https://www.linkedin.com/in/abdul-razak-sarkwah?utm_source=share_via&utm_content=profile&utm_medium=member_android" className="linkedin-link">
                         <img src="/linkedin.png" alt="LinkedIn icon" />
                         <span>LinkedIn</span>
                     </a>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
